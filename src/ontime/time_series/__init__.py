f9042be6bf033646d2f16950b78a9f6ac6a093b9
Fred Montet <fredmontet@gmail.com>
2023-10-30 16:34:32 +0100

    Removed conflict tags

--- src/ontime/time_series/__init__.py
+++ src/ontime/time_series/__init__.py
@@ -1,7 +1,4 @@
 from .time_series import TimeSeries
 from .probabilistic_time_series import ProbabilisticTimeSeries
 from .binary_time_series import BinaryTimeSeries
-<<<<<<< HEAD
-=======
 from .resticted_time_series import RestrictedTimeSeries
->>>>>>> VincentMagn/develop
