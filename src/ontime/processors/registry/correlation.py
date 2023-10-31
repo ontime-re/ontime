94973c7ba8e5a35c5861e2f9df905b74db5d4651
Fred Montet <fredmontet@gmail.com>
2023-10-31 09:14:00 +0100

    Format with Black

--- src/ontime/processors/registry/correlation.py
+++ src/ontime/processors/registry/correlation.py
@@ -5,11 +5,11 @@ from ...time_series import TimeSeries
 
 
 class Correlation:
-    """ Correlation class handles correlation computation in a TimeSeries """
+    """Correlation class handles correlation computation in a TimeSeries"""
 
     @staticmethod
     def process(ts, window):
-        """ Compute correlations for a TimeSeries
+        """Compute correlations for a TimeSeries
 
         :param ts: TimeSeries
         :param window: int, timedelta, str, offset, or BaseIndexer subclass
@@ -22,7 +22,6 @@ class Correlation:
         df = Correlation.pivot(df)
         df.columns.name = None  # Otherwise, the column name is 'pair' and from_dataframe() fails in the next line
         return TimeSeries.from_dataframe(df)
-
 
     @staticmethod
     def compute_correlations(df, window):
@@ -48,18 +47,24 @@ class Correlation:
 
             # Step 3: Extract upper triangle of the correlation matrix without the diagonal
             # Since indices and columns are the same, we can assume it's a square matrix
-            upper_triangle_indices = np.triu_indices(n=current_df.shape[0], k=1)  # k=1 excludes the main diagonal
+            upper_triangle_indices = np.triu_indices(
+                n=current_df.shape[0], k=1
+            )  # k=1 excludes the main diagonal
             upper_triangle_values = current_df.values[upper_triangle_indices]
 
             # Step 4: Store non-redundant items with their respective labels and time
             # We iterate directly over the upper_triangle_indices, which are pairs of (row, col) positions in the matrix
-            for (row, col), value in zip(zip(*upper_triangle_indices), upper_triangle_values):
-                non_redundant_items.append({
-                    'time': time,
-                    'var_a': current_df.index[row],
-                    'var_b': current_df.columns[col],
-                    'correlation': value
-                })
+            for (row, col), value in zip(
+                zip(*upper_triangle_indices), upper_triangle_values
+            ):
+                non_redundant_items.append(
+                    {
+                        "time": time,
+                        "var_a": current_df.index[row],
+                        "var_b": current_df.columns[col],
+                        "correlation": value,
+                    }
+                )
 
         # Step 5: Convert the list of non-redundant items to a new DataFrame
         result_df = pd.DataFrame(non_redundant_items)
@@ -75,14 +80,14 @@ class Correlation:
         """
 
         # Step 1: Set 'time' as the index
-        df.set_index('time', inplace=True)
+        df.set_index("time", inplace=True)
 
         # Step 2: Create a unique identifier for each (row_index, col_index) pair
         # This will become the column names in the reshaped DataFrame
-        df['pair'] = df['var_a'] + '_' + df['var_b']
+        df["pair"] = df["var_a"] + "_" + df["var_b"]
 
         # Step 3: Pivot the table so that each 'pair' becomes a column
         # The values in the table will be the 'correlation' values
-        pivoted_df = df.pivot(columns='pair', values='correlation')
+        pivoted_df = df.pivot(columns="pair", values="correlation")
 
         return pivoted_df
