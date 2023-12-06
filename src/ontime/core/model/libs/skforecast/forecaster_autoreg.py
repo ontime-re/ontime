ce02ade9d11d89c3c85112f8b160c76980e4d657
vincent magnin <vincent.magnin@hes-so.ch>
2023-12-06 08:10:50 +0100

    Add comments as requested on PR

--- src/ontime/core/model/libs/skforecast/forecaster_autoreg.py
+++ src/ontime/core/model/libs/skforecast/forecaster_autoreg.py
@@ -12,15 +12,34 @@ class ForecasterAutoreg(AbstractModel):
     """
 
     def __init__(self, model, **params):
+        """
+        Initialize model based on ForecasterAutoreg. **params are defined in ForecasterAutoreg from sklearn
+        """
         super().__init__()
         if isinstance(model, ABCMeta):
             model = model()
         self.model = SkForecastForecasterAutoreg(regressor=model, **params)
 
     def fit(self, ts: TimeSeries, **params) -> "ForecasterAutoreg":
+        """
+        Fit model SkForecasterAutoreg, params are defined in ForecasterAutoreg from sklearn
+
+        :param ts: TimeSeries used to fit the model
+        :param params: params used to fit the model documented in ForecasterAutoreg from sklearn
+
+        Return the model fitted
+        """
         self.model.fit(y=ts.pd_series(), **params)
         return self
 
     def predict(self, n: int, **params) -> TimeSeries:
+        """
+        Predict n steps into the future, must match with the limit defined on model initialization
+
+        :param n: int number of steps to predict
+        :param params: params used to predict the model documented in ForecasterAutoreg from sklearn
+
+        Return the prediction in a TimeSeries
+        """
         pred = self.model.predict(n, **params)
         return TimeSeries.from_series(pred)
--- src/ontime/core/model/libs/skforecast/forecaster_autoreg_multi_variate.py
+++ src/ontime/core/model/libs/skforecast/forecaster_autoreg_multi_variate.py
@@ -12,16 +12,36 @@ class ForecasterAutoregMultiVariate(AbstractModel):
     """
 
     def __init__(self, model, **params):
+        """
+        Initialize model based on ForecasterAutoregMultiVariate. **params are defined in ForecasterAutoregMultiVariate
+        from sklearn
+        """
         super().__init__()
         if isinstance(model, ABCMeta):
             model = model()
         self.model = SkForecasterAutoregMultiVariate(regressor=model, **params)
 
     def fit(self, ts: TimeSeries, **params) -> "ForecasterAutoregMultiVariate":
+        """
+        Fit model SkForecasterAutoregMultiVariate, params are defined in ForecasterAutoregMultiVariate from sklearn
+
+        :param ts: TimeSeries used to fit the model
+        :param params: params used to fit the model documented in ForecasterAutoregMultiVariate from sklearn
+
+        Return the model fitted
+        """
         df = ts.pd_dataframe()
         self.model.fit(series=df, **params)
         return self
 
     def predict(self, n: int, **params) -> TimeSeries:
+        """
+        Predict n steps into the future, must match with the limit defined on model initialization
+
+        :param n: int number of steps to predict must match with the limit defined on model initialization
+        :param params: params used to predict the model documented in ForecasterAutoregMultiVariate from sklearn
+
+        Return the prediction in a TimeSeries
+        """
         pred = self.model.predict(n, **params)
         return TimeSeries.from_dataframe(pred)
