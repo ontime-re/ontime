96db5ea7f964afdeef639412eddbfa8436c0c141
Fred Montet <fredmontet@gmail.com>
2024-01-24 21:07:31 +0100

    Formatted run file

--- notebooks/run.py
+++ notebooks/run.py
@@ -1,12 +1,9 @@
 import papermill as pm
 from pathlib import Path
 
-current_directory = Path('.')
-files = [file for file in current_directory.rglob('*.ipynb')]
+current_directory = Path(".")
+files = [file for file in current_directory.rglob("*.ipynb")]
 
 for file in files:
-    print(f'Executing : {file}')
-    pm.execute_notebook(
-        input_path=file,
-        output_path=file
-    )
+    print(f"Executing : {file}")
+    pm.execute_notebook(input_path=file, output_path=file)
