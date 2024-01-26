2d69150174180b089a6926701f51827e016e4466
Fred Montet <fredmontet@gmail.com>
2024-01-26 21:51:29 +0100

    formatted script

--- notebooks/run.py
+++ notebooks/run.py
@@ -13,7 +13,7 @@ print("Files to execute:\n")
 print(files)
 
 for file in files:
-    #print("\n")
+    # print("\n")
     print(f"Executing: {file}")
     pm.execute_notebook(input_path=file, output_path=file, progress_bar=False)
     print(f"Finished: {file}")
