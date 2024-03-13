b279e37373a7008e92e3d62f4c92f3d0fc3d83cf
Charlie Junod <charlie.junod@hefr.ch>
2024-03-13 13:27:16 +0100

    added an abstract metric class in benchmark so that users can submit their own. The benchmark results can be exported as dataframes as well now.

--- src/ontime/module/benchmarking/benchmark.py
+++ src/ontime/module/benchmarking/benchmark.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ontime.module.data.dataset import Dataset as dl
 from ontime.core.time_series.time_series import TimeSeries
 from ontime.core.model.model import Model
@@ -8,23 +9,29 @@ import time
 import copy
 from typing import List, Union, Tuple
 import traceback
+from abc import ABC, abstractmethod
+
 
 class Benchmark:
     class BenchmarkDataset:
-        def __init__(self, training_set: TimeSeries, multivariate, name, test_set: TimeSeries = None):
+        def __init__(self, training_set: TimeSeries, multivariate: bool = None, name: str = None,
+                     test_set: TimeSeries = None):
             self.training_set = training_set
             self.test_set = test_set
+            if multivariate is None:
+                multivariate = len(training_set.columns) > 1
             self.multivariate = multivariate
             self.name = name
 
-        def get_datasets(self, test_proportion=0.3):
+        def get_datasets(self, train_proportion=0.7):
             if self.test_set is None:
-                return self.training_set.split_before(test_proportion)
+                return self.training_set.split_before(train_proportion)
             return self.training_set, self.test_set
 
     class BenchmarkModel(AbstractModel):
-        def __init__(self, model):
+        def __init__(self, model, name: str = None):
             self.model = model
+            self.name = name
 
         def fit(self, training_set: TimeSeries):
             self.model.fit(training_set)
@@ -32,10 +39,23 @@ class Benchmark:
         def predict(self, horizon, test_set):
             return self.model.predict(horizon)
 
-    def __init__(self, datasets: List[Union[TimeSeries, Tuple[TimeSeries, TimeSeries]]] = None,
-                 models: List[AbstractModel] = None):
+    class BenchmarkMetric:
+        def __init__(self, function, name):
+            self.metric = function
+            self.name = name
+
+        def compute(self, data: TimeSeries, pred: TimeSeries):
+            return self.metric(data, pred)
+
+    def __init__(self, datasets: List[Union[TimeSeries, Tuple[TimeSeries, TimeSeries], BenchmarkDataset]] = None,
+                 models: List[AbstractModel] = None,
+                 metrics: List[BenchmarkMetric] = None):
         self.datasets = []
         self.models = []
+        self.metrics = []
+        self.metrics_results = []
+        if metrics is not None:
+            self.metrics = metrics
         if datasets is not None:
             i = 1
             for d in datasets:
@@ -44,59 +64,65 @@ class Benchmark:
         if models is not None:
             i = 1
             for m in models:
-                self.add_model(m, str(i))
-                i += 1
+                self.add_model(m)
         self.results = None
 
-    def add_model(self, model: AbstractModel, name: str = None):
+    def add_model(self, model: AbstractModel):
         if not isinstance(model, AbstractModel):
             raise TypeError(f"models must implement {AbstractModel}")
-        if name is None:
-            name = str(len(self.models))
-        self.models.append({
-            'model': model,
-            'name': name
-        })
+        if model.name is None:
+            model.name = str(len(self.models))
+        self.models.append(model)
 
     def get_models(self):
         return [m['model'] for m in self.models]
 
-    def add_dataset(self, dataset: Union[TimeSeries, Tuple[TimeSeries, TimeSeries]], name: str = None):
-        if not isinstance(dataset, TimeSeries) and not isinstance(dataset, (TimeSeries, TimeSeries)):
+    def add_dataset(self, dataset: Union[TimeSeries, Tuple[TimeSeries, TimeSeries], BenchmarkDataset], name=None):
+        if not (isinstance(dataset, TimeSeries) or
+                isinstance(dataset, Tuple) or
+                isinstance(dataset, Benchmark.BenchmarkDataset)):
             raise TypeError(
-                f"datasets must be of type {TimeSeries} or {(TimeSeries, TimeSeries)}, found type {type(dataset)}")
+                f"datasets must be of type {Benchmark.BenchmarkDataset}, {TimeSeries} or {(TimeSeries, TimeSeries)}, "
+                f"found type {type(dataset)}")
         multivariate = True
-        if len(dataset.columns) < 2:
+        if (isinstance(dataset, Benchmark.BenchmarkDataset) and len(dataset.training_set.columns) < 2) \
+                or (isinstance(dataset, TimeSeries) and len(dataset.columns) < 2) \
+                or (isinstance(dataset, Tuple) and len(dataset[0].columns) < 2):
             multivariate = False
+
         if name is None:
             name = str(len(self.models))
         if isinstance(dataset, TimeSeries):
             self.datasets.append(Benchmark.BenchmarkDataset(dataset, multivariate, name))
-        else:
+        elif isinstance(dataset, Tuple):
             self.datasets.append(Benchmark.BenchmarkDataset(dataset[0], multivariate, name, dataset[1]))
+        else:  # it's a BenchmarkDataset
+            self.datasets.append(dataset)
 
     def get_datasets(self):
         return [d for d in self.datasets]
 
     def run(self, test_proportion: float = 0.3, verbose: bool = False):
         # TODO: throw error if models or datasets is empty
-        self.results = []
+        self.results = {}  # self.results[model name][dataset name] = {metric name: value, metric name: value,...}
         if verbose:
             print("Starting evaluation...")
         i = 0
         for source_model in self.models:
-            self.results.append([])
+            self.results[source_model.name] = {}
             if verbose:
-                print(f"Evaluation for model {source_model['name']}")
-            results_i = pd.DataFrame(
-                index=['nb features', 'train size', 'train time', 'test size', 'test time', 'mape'])
+                print(f"Evaluation for model {source_model.name}")
+
+            # internal metrics (computed during run time)
             nb_mv_run_failed = len([x for x in self.datasets if x.multivariate])
             nb_uv_run_failed = len(self.datasets) - nb_mv_run_failed
             mv0 = len([x for x in self.datasets if x.multivariate])
+
+            # evaluation time!
             for dataset in self.datasets:
                 if verbose:
                     print(f"on dataset {dataset.name} ")
-                model = copy.deepcopy(source_model['model'])
+                model = copy.deepcopy(source_model)
                 train_set, test_set = dataset.get_datasets()
                 nb_features = len(train_set.columns)
                 train_size = len(train_set.time_index)
@@ -117,8 +143,17 @@ class Benchmark:
                     inference_time = time.time() - start_time
                     if verbose:
                         print(f"done, took {inference_time}")
-                    mape = metrics.mape(test_set, pred)
-                    results_i[dataset.name] = [nb_features, train_size, train_time, test_size, inference_time, mape]
+
+                    self.results[source_model.name][dataset.name] = {
+                        'nb features': nb_features,
+                        'training set size': train_size,
+                        'training time': train_time,
+                        'test set size': test_size,
+                        'testing time': inference_time
+                    }
+                    for metric in self.metrics:
+                        # print(f'----------------------------------{metric.name}')
+                        self.results[source_model.name][dataset.name][metric.name] = metric.compute(test_set, pred)
                 except:  # can't train on this dataset
                     if dataset.multivariate:
                         nb_mv_run_failed -= 1
@@ -126,7 +161,7 @@ class Benchmark:
                         nb_uv_run_failed -= 1
                     if verbose:
                         print(f"Couldn't complete training.")
-                        traceback.print_exc()
+                        # traceback.print_exc()
 
             supports_uni = None
             if len(self.datasets) - mv0 > 0:  # if we have univariate ds in our batch
@@ -134,42 +169,54 @@ class Benchmark:
             supports_mult = None
             if mv0 > 0:
                 supports_mult = (nb_mv_run_failed > 0)
-            self.results[i] = {
-                'metrics': results_i,
-                'supports univariate': supports_uni,
-                'supports multivariate': supports_mult
-            }
+            self.results[source_model.name]['supports univariate'] = Benchmark._bool_to_symbol(supports_uni)
+            self.results[source_model.name]['supports multivariate'] = Benchmark._bool_to_symbol(supports_uni)
             i += 1
         # return self.results
+
+    def _bool_to_symbol(b):
+        if b is None:
+            return 'unknown'
+        if b:
+            return '✓'
+        return 'X'
 
     def get_report(self):
         if self.results is None:
             return "please invoke run_benchmark() to generate report data"
         txt = []
 
-        for i in range(0, len(self.models)):
-            suni = self.results[i]['supports univariate']
-            smul = self.results[i]['supports multivariate']
-            match suni:
-                case True:
-                    suni = '✓'
-                case False:
-                    suni = 'X'
-                case None:
-                    suni = "unknown"
-            match smul:
-                case True:
-                    smul = '✓'
-                case False:
-                    smul = 'X'
-                case None:
-                    smul = "unknown"
-            model = self.models[i]
-            s = f"Model {model['name']}:\n"
-            s += f"Supported univariate datasets: {suni}\n"
-            s += f"Supported multivariate datasets: {smul}\n"
-            s += f"{self.results[i]['metrics']}\n"
+        for model in self.models:
+            s = f"Model {model.name}:\n"
+            s += f"Supported univariate datasets: {self.results[model.name]['supports univariate']}\n"
+            s += f"Supported multivariate datasets: {self.results[model.name]['supports multivariate']}\n"
+            for dataset in self.datasets:
+                s += f"Dataset {dataset.name}:\n"
+                if dataset.name not in self.results[model.name].keys():
+                    s += f"{dataset.name}: couldn't complete training\n"
+                else:
+                    for key in self.results[model.name][dataset.name].keys():
+                        s += f"{key}: {self.results[model.name][dataset.name][key]}\n"
 
             txt.append(s)
         report = "\n".join(txt)
         return report
+
+    def get_report_dataframes(self) -> dict:
+        # 1 dataframe per model
+        # index = datasets
+        # columns = metric
+        ds_index = [x for x in self.results[self.models[0].name].keys() if
+                    'supports' not in x]  # remove model-only stats
+        me_columns = self.results[self.models[0].name][self.datasets[0].name].keys()
+        reports = {}
+        for model in self.models:
+            report_model = pd.DataFrame(columns=me_columns, index=ds_index)
+            for dataset in self.datasets:
+                if dataset.name not in self.results[model.name].keys():
+                    report_dict = {n: np.nan for n in me_columns}
+                else:
+                    report_dict = self.results[model.name][dataset.name]
+                report_model.loc[dataset.name] = report_dict
+            reports[model.name] = report_model
+        return reports
