af7961990551a33cda693f614f6021eed1ce49dc
Fred Montet <fredmontet@gmail.com>
2025-03-06 14:13:47 +0100

    Added forgotten lines in init

--- src/ontime/core/processing/__init__.py
+++ src/ontime/core/processing/__init__.py
@@ -3,11 +3,12 @@ from .registry.filler import Filler
 from .registry.mapper import Mapper
 from .registry.windower import Windower
 from .registry.correlation import Correlation
-
+from .registry.density import Density
 processors = Processors()
 processors.load("filler", Filler)
 processors.load("mapper", Mapper)
 processors.load("windower", Windower)
 processors.load("correlation", Correlation)
+processors.load("density", Density)
 
 __all__ = ["processors"]
