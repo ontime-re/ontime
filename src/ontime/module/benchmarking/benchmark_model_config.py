e410976a8bbd8f5e8e349c537a625a15112aa0c0
Benjamin Pasquier <benjamin.pasquier@hefr.ch>
2025-09-24 16:54:15 +0200

    rename arguments for coherence, add a argument for dynamic fit params as well and update docs accordingly

--- src/ontime/module/benchmarking/benchmark_model_config.py
+++ src/ontime/module/benchmarking/benchmark_model_config.py
@@ -25,7 +25,8 @@ class BenchmarkModelConfig:
         benchmark_mode: Optional[BenchmarkMode] = None,
         static_model_params: Optional[Dict[str, Any]] = None,
         dynamic_model_params: Optional[Dict[str, Callable[[BenchmarkDataset], Any]]] = None,
-        dynamic_predict_kwargs: Optional[Dict[str, Callable[[BenchmarkDataset], Any]]] = None,
+        dynamic_predict_params: Optional[Dict[str, Callable[[BenchmarkDataset], Any]]] = None,
+        dynamic_fit_params: Optional[Dict[str, Callable[[BenchmarkDataset], Any]]] = None,
         validation_set_param: Optional[str] = None,
     ):
         """
@@ -36,7 +37,9 @@ class BenchmarkModelConfig:
         :param benchmark_mode: DEPRECATED - either zero shot or full shot
         :param static_model_params: dictionary of model parameters that are static, known as soon as the model is declared
         :param dynamic_model_params: dictionary of model parameters that are functions depending on the dataset
-        :param dynamic_predict_kwargs: dictionary of additional parameters for model predict method,
+        :param dynamic_predict_params: dictionary of additional parameters for model predict method,
+        that are functions depending on the dataset
+        :param dynamic_fit_params: dictionary of additional parameters for model fit method,
         that are functions depending on the dataset
         :param validation_set_param: name of the parameter for the validation set to give to the model fit method
         :return: the initialized BenchmarkModelConfig
@@ -48,7 +51,9 @@ class BenchmarkModelConfig:
         self.validation_set_param = validation_set_param
         self._static_model_params = static_model_params or {}
         self._dynamic_model_params = dynamic_model_params or {}
-        self._dynamic_predict_kwargs = dynamic_predict_kwargs or {}
+        self._dynamic_predict_kwargs = dynamic_predict_params or {}
+        self._dynamic_fit_kwargs = dynamic_fit_params or {}
+
 
         # depreciation about benchmark_mode
         if benchmark_mode is not None:
@@ -98,4 +103,12 @@ class BenchmarkModelConfig:
         :return: the resolved prediction kwargs
         """
         return self._resolve_dynamic_params(self._dynamic_predict_kwargs, dataset)
+
+    def get_fit_kwargs(self, dataset: BenchmarkDataset):
+        """
+        Returns the resolved dynamic kwargs for model fit method.
+        :param dataset: The dataset being used to compute dynamic kwargs.
+        :return: the resolved prediction kwargs
+        """
+        return self._resolve_dynamic_params(self._dynamic_fit_kwargs, dataset)
 
